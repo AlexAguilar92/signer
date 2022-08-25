c166e7aa9c4d25ca5e051efefb39728d97da6ca9
Alexandro Aguilar <aleaguilar@palaceresorts.com>
2022-08-25 15:44:28 -0500

    implementation

--- src/modules/sign/useCase/SignCreateUseCase.ts
+++ src/modules/sign/useCase/SignCreateUseCase.ts
@@ -1,11 +1,23 @@
 import 'reflect-metadata';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import UseCase from "../../common/useCase/UseCase";
-import SignCreateUseCaseParams from "../domain/DTO/SignCreateUseCaseParams.dto";
+// import SignCreateUseCaseParams from "../domain/DTO/SignCreateUseCaseParams.dto";
+import TYPES from '../../../types';
+// import SignCreateRepositoryQuantum from '../domain/repository/SignCreateRepositoryQuantum';
+import Repository from '../../common/domain/repository/Repository';
+import Document from '../domain/entity/Document';
 
 @injectable()
-export default class SignCreateUseCase implements UseCase<SignCreateUseCaseParams, string> {
-  async execute(port: SignCreateUseCaseParams): Promise<string> {
-    return '1'
+export default class SignCreateUseCase implements UseCase<Partial<Document>, string> {
+  private signCreateRepositoryQuantum: Repository<Partial<Document>, string>
+
+  constructor (
+    @inject(TYPES.SignCreateRepository) signCreateRepositoryQuantum: Repository<Document, string>
+  ) {
+    this.signCreateRepositoryQuantum = signCreateRepositoryQuantum;
+  }
+
+  async execute(port: Partial<Document>): Promise<string> {
+    return await this.signCreateRepositoryQuantum.execute(port)
   }
 }
