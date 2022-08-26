efd9ddd939484d34086e9463426f7f17d965f3f6
Alexandro Aguilar <aleaguilar@palaceresorts.com>
2022-08-26 09:49:30 -0500

    base64 to template

--- src/modules/sign/domain/entity/Document.ts
+++ src/modules/sign/domain/entity/Document.ts
@@ -1,8 +1,53 @@
 import DefaultEntity from "../../../common/domain/entity/DefaultEntity";
 
 export default class Document extends DefaultEntity {
-  overrideMinimumRequiredLevel: string;
-  fact: object;
-  pipeline: object;
-  template: object;
+  private _overrideMinimumRequiredLevel: string;
+  private _fact: object;
+  private _pipeline: object;
+  private _template: string;
+
+  constructor(
+    overrideMinimumRequiredLevel: string,
+    fact: object,
+    pipeline: object,
+    template: string
+  ) {
+    super()
+    this._overrideMinimumRequiredLevel = overrideMinimumRequiredLevel
+    this._fact = fact
+    this._pipeline = pipeline
+    this._template = Buffer.from(template).toString('base64')
+  }
+
+  get overrideMinimumRequiredLevel() {
+    return this._overrideMinimumRequiredLevel;
+  }
+
+  get fact() {
+    return this._fact;
+  }
+
+  get pipeline() {
+    return this._pipeline;
+  }
+
+  get template() {
+    return this._template;
+  }
+
+  set overrideMinimumRequiredLevel(overrideMinimumRequiredLevel: string) {
+    this._overrideMinimumRequiredLevel = overrideMinimumRequiredLevel;
+  }
+
+  set fact(fact: object) {
+    this._fact = fact;
+  }
+
+  set pipeline(pipeline: object) {
+    this._pipeline = pipeline;
+  }
+
+  set template(template: string) {
+    this._template = template;
+  }
 }
