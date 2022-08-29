c2681910906954e5f7d14c71696e70e6de576213
Alex Aguilar <auza920205@gmail.com>
2022-08-28 19:47:50 -0500

    linter added

--- services/schemas/Sign.schema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi'
-
-export default Joi.object().keys({
-  overrideMinimumRequiredLevel: Joi.string().required(),
-  fact: Joi.object().required(),
-  pipeline: Joi.string().required(),
-  template: Joi.string().required()
-}).required()
