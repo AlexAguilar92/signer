b35110763f1951fec5b7d49cac07e51e6addb49c
Alexandro Aguilar <aleaguilar@palaceresorts.com>
2022-08-29 17:08:37 -0500

    Updated

--- src/modules/common/domain/entity/DefaultEntity.ts
+++ src/modules/common/domain/entity/DefaultEntity.ts
@@ -1,13 +1,52 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default class DefaultEntity {
-  id: string;
+  private _id: string;
+  private _createdAt: Date;
+  private _createdBy: string;
+  private _active: boolean;
 
-  createdAt: Date;
+  constructor (
+    createdBy: string,
+    id: string = uuidv4(),
+    createdAt: Date = new Date(),
+    active: boolean = true
+  ) {
+    this._id = id;
+    this._createdAt = createdAt;
+    this._createdBy = createdBy;
+    this._active = active;
+  }
 
-  createdBy: string;
+  get id(): string {
+    return this._id;
+  }
 
-  updatedAt: Date;
+  set id(id: string) {
+    this._id = id;
+  }
 
-  updatedBy: string;
+  get createdAt(): Date {
+    return this._createdAt;
+  }
 
-  active: boolean;
+  set createdAt(createdAt: Date) {
+    this._createdAt = createdAt;
+  }
+
+  get createdBy(): string {
+    return this._createdBy;
+  }
+
+  set createdBy(createdBy: string) {
+    this._createdBy = createdBy;
+  }
+
+  get active(): boolean {
+    return this._active;
+  }
+
+  set active(active: boolean) {
+    this._active = active;
+  }
 }
