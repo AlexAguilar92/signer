d9f74451dc7d9119c1a9940c8f32f17139ce41eb
Alexandro Aguilar <aleaguilar@palaceresorts.com>
2022-08-30 10:02:05 -0500

    deleted useless properties

--- src/modules/common/domain/entity/DefaultEntity.ts
+++ src/modules/common/domain/entity/DefaultEntity.ts
@@ -1,37 +1,15 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export default class DefaultEntity {
-  private _id: string;
-  private _createdAt: Date;
   private _createdBy: string;
   private _active: boolean;
 
   constructor (
     createdBy: string,
-    id: string = uuidv4(),
-    createdAt: Date = new Date(),
     active: boolean = true
   ) {
-    this._id = id;
-    this._createdAt = createdAt;
     this._createdBy = createdBy;
     this._active = active;
-  }
-
-  get id(): string {
-    return this._id;
-  }
-
-  set id(id: string) {
-    this._id = id;
-  }
-
-  get createdAt(): Date {
-    return this._createdAt;
-  }
-
-  set createdAt(createdAt: Date) {
-    this._createdAt = createdAt;
   }
 
   get createdBy(): string {
