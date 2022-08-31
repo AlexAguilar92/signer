b33828a7f8f13b0ad40ac9de4499a513d37e133c
Alexandro Aguilar <aleaguilar@palaceresorts.com>
2022-08-31 17:46:46 -0500

    added support for overrides

--- src/schemas/Registration.schema.ts
+++ src/schemas/Registration.schema.ts
@@ -4,5 +4,6 @@ export default Joi.object().keys({
   overrideMinimumRequiredLevel: Joi.string().required(),
   fact: Joi.object().required(),
   pipeline: Joi.string().required(),
+  overrides: Joi.object().required(),
   template: Joi.string().required()
 }).required()
