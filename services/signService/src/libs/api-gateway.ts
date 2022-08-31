662f3fb5bfb2c942a61c04e2942a99e8dc5008af
Alexandro Aguilar <aleaguilar@palaceresorts.com>
2022-08-31 12:47:42 -0500

    moved

--- services/signService/src/libs/api-gateway.ts
+++
@@ -1,8 +0,0 @@
-import HttpStatusCode from "../../../../src/shared/enums/httpStatusCode";
-
-export const formatJSONResponse = (data, statusCode: HttpStatusCode = HttpStatusCode.OK) => {
-  return {
-    data,
-    statusCode
-  }
-}
