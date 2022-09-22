07f24b7dfb0ce50a1706db6ff3341aa3d635fa5c
Alex Aguilar <aleaguilar@palaceresorts.com>
2022-09-22 16:12:11 -0500

    ordered imports

--- services/signService/src/functions/register/handler.ts
+++ services/signService/src/functions/register/handler.ts
@@ -1,16 +1,16 @@
 import 'reflect-metadata';
 import middy from '@middy/core';
+import container from './inversify.config';
+import TYPES from '../../../../../src/types';
+import { formatJSONResponse } from '../../libs/api-gateway';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
 import httpResponseHandlerMiddleware from '../../../../../src/middleware/httpResponseHandlerMiddleware';
 import httpRequestHandlerMiddleware from '../../../../../src/middleware/httpRequestHandlerMiddleware';
 import {httpJoiValidatorMiddleware, VALIDATOR_TYPE} from '../../../../../src/middleware/httpJoiValidatorMiddleware'
 import HttpStatusCode from '../../../../../src/shared/enums/httpStatusCode';
 import Registration from '../../../../../src/schemas/Registration.schema'
-import container from './inversify.config';
 import Adapter from '../../../../../src/modules/common/adapter/Adapter';
 import SignCreateAdapterParams from '../../../../../src/modules/register/domain/DTO/SignCreateAdapterParams.dto';
-import TYPES from '../../../../../src/types';
-import { formatJSONResponse } from '../../libs/api-gateway';
 
 const main = middy(async event => {
   const adapter: Adapter<SignCreateAdapterParams, string> = container.get<Adapter<SignCreateAdapterParams, string>>(TYPES.SignCreateAdapter);
