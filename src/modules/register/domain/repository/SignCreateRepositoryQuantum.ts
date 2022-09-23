04f3ce429cbb79e12cac5e539b2c03c067a88864
Alex Aguilar <aleaguilar@palaceresorts.com>
2022-09-23 18:12:05 -0500

    updated

--- src/modules/register/domain/repository/SignCreateRepositoryQuantum.ts
+++ src/modules/register/domain/repository/SignCreateRepositoryQuantum.ts
@@ -1,10 +1,10 @@
 import 'reflect-metadata'
-import { QldbDriver, TransactionExecutor } from "amazon-qldb-driver-nodejs";
-import { inject, injectable } from "inversify";
-import DBConnectionManager from "../../../../shared/database/DBConnectionManager";
 import TYPES from "../../../../types";
-import Repository from "../../../common/domain/repository/Repository";
 import Document from "../entity/Document";
+import { inject, injectable } from "inversify";
+import Repository from "../../../common/domain/repository/Repository";
+import { QldbDriver, TransactionExecutor } from "amazon-qldb-driver-nodejs";
+import DBConnectionManager from "../../../../shared/database/DBConnectionManager";
 
 @injectable()
 export default class SignCreateRepositoryQuantum implements Repository<Partial<Document>, string> {
@@ -18,13 +18,15 @@ export default class SignCreateRepositoryQuantum implements Repository<Partial<D
   
   async execute(document: Partial<Document>): Promise<string> {
     const connection = await this.dBConnectionManagerQuantum.connect();
+    // console.log('document.data:', document.getData())
     try {
       const result = await connection.executeLambda(async (txn: TransactionExecutor) => {
-        return await txn.execute("INSERT INTO Documents ?", document);
+        return await txn.execute("INSERT INTO Documents ?", document.getData());
       });
+      // console.log('SignCreateRepositoryQuantum result', result);
       return result.getResultList().at(0).fields().at(0).at(1).toString();
     } catch (error) {
-      console.log(error);
+      console.log('SignCreateRepositoryQuantum error', error);
       throw error;
     } finally {
       connection.close();
