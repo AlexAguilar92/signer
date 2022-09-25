4b74bfa5d5dd74b54ffe8fa4d081fddb9d24ab86
Alex Aguilar <aleaguilar@palaceresorts.com>
2022-09-25 11:38:56 -0500

    updated httpstatuscode depending on response

--- services/signService/src/functions/register/handler.ts
+++ services/signService/src/functions/register/handler.ts
@@ -11,13 +11,15 @@ import HttpStatusCode from '../../../../../src/shared/enums/httpStatusCode';
 import Registration from '../../../../../src/schemas/Registration.schema'
 import Adapter from '../../../../../src/modules/common/adapter/Adapter';
 import SignCreateAdapterParams from '../../../../../src/modules/register/domain/DTO/SignCreateAdapterParams.dto';
+import Document from '../../../../../src/modules/register/domain/entity/Document';
 
 const main = middy(async event => {
   const adapter: Adapter<SignCreateAdapterParams, string> = container.get<Adapter<SignCreateAdapterParams, string>>(TYPES.SignCreateAdapter);
-  const response = await adapter.execute({...event.body, user: event.user});
+  const response: Document | string = await adapter.execute({...event.body, user: event.user});
+
   return formatJSONResponse(
-      response,
-      HttpStatusCode.CREATED
+    response,
+    response as unknown instanceof Document ?  HttpStatusCode.OK : HttpStatusCode.CREATED
   );
 });
 
